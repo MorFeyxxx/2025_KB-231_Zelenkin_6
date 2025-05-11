1f5b9e1e7ab69ff92628f1a2f592cb4698dd92cb
Morfey <stalker22813372@mail.ru>
2025-05-11 14:04:57 +0300

    Add interface SmartPtr

--- SmartPtr.h
+++ SmartPtr.h
@@ -1,4 +1,19 @@
 #ifndef SMARTPTR_H
 #define SMARTPTR_H
 
+template <typename T>
+class SmartRtr {
+    T* obj;
+public:
+    SmartRtr(T* obj) : obj(obj) {}
+    ~SmartRtr() { delete obj; }
+
+    T* operator->() { return obj; }
+    T& operator*() { return *obj; }
+
+    // Запрет копирования
+    SmartRtr(const SmartRtr&) = delete;
+    SmartRtr& operator=(const SmartRtr&) = delete;
+};
+
 #endif // SMARTPTR_H
